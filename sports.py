723c09872adb229e454e75c127f46f2efe7851ad
Druhi <druhi.vid@gmail.com>
2024-01-05 21:54:57 +0530

    for matching case

---
+++ sports.py
@@ -0,0 +1,16 @@
+i = input("sport : ")
+match i:
+    case("lawn tennis"):
+        print("druhi,vismaya,navya,amaira,sehej,swastika,samriddhi")
+    case("football"):
+        print("shaurya")
+    case("karate"):
+        print("adira,aliya,anaya,pranshi")
+    case("skating"):
+        print("ayaan,atharv")
+    case("cricket"):
+        print("meher")
+    case("basketball"):
+        print("kaven")
+    case(unknown):
+        print("unknown")
