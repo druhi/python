25d2e839d3ffec2b0cc0a10745a2a1cc59c6ecef
Druhi <druhi.vid@gmail.com>
2024-11-11 21:15:08 +0530

    to pratcise dictionaries that are nested

---
+++ w.py
@@ -0,0 +1,14 @@
+d = {'druhi' : 
+        {'last_name':'jukaria',
+        'city':'noida'}
+}
+
+y = {'yajat' :
+         {'last name' : 'oli',
+          "city" : 'kuala lampur'}
+        }
+print(f"druhi - {d}")
+print(f"yajat - {y}")
+
+
+
