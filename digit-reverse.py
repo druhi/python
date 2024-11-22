4660fb99e941ad60eea0c0af6f015a16cbf390ad
Druhi <druhi.vid@gmail.com>
2024-11-22 21:52:18 +0530

    to practise

--- digit-reverse.py
+++ digit-reverse.py
@@ -11,14 +11,14 @@ for i in range(x):
     x = x -1
 print()
 ##############################
-n = 54321
-num = n
-reverse = 0
-while(num!=0):
-    digit = num%10
-    reverse = reverse*10+ digit
-    num=int(num/10)
-print(f"reverse of {n} is {reverse}")
+# n = 54321
+# num = n
+# reverse = 0
+# while(num!=0):
+#     digit = num%10
+#     reverse = reverse*10+ digit
+#     num=int(num/10)
+# print(f"reverse of {n} is {reverse}")
 
     
 
