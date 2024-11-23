f9b336592eb4f4ae2ee5497fb1101894c42b6fc0
Druhi <druhi.vid@gmail.com>
2024-11-23 21:06:09 +0530

    to practise

---
+++ 3-4orielly.py
@@ -0,0 +1,10 @@
+guest = ["aunt","uncle","sister"]
+print(f"dear{guest[0]} you are invited for our birthday")
+print(f"dear{guest[1]} you are invited for our birthday")
+print(f"dear{guest[2]} you are invited for our birthday")
+print("sister has imfomed she is not attending")
+guest.remove(guest[2])
+guest.append("niece")
+print(f"dear{guest[0]} you are invited for our birthday")
+print(f"dear{guest[1]} you are invited for our birthday")
+print(f"dear{guest[2]} you are invited for our birthday")
