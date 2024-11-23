cbf4d01cd997ef64f86cd3de9f6ae2861bb3f2d8
Druhi <druhi.vid@gmail.com>
2024-11-23 21:05:43 +0530

    to practise

---
+++ 5-4orielly.py
@@ -0,0 +1,8 @@
+alien_color = ["red","green","yellow"]
+if alien_color == "green":
+    print("you have earned 5 points")
+if alien_color >= "green":
+    print("you have earned 10 points")
+
+    
+    
