d35ebb3efc35b73c9657949bdf77f3d0cafe7fe5
Druhi <druhi.vid@gmail.com>
2024-11-23 21:09:01 +0530

    to practise

--- genderise+nationlize.py
+++ genderise+nationlize.py
@@ -2,17 +2,6 @@ import requests
 import json
 # import library
 name = input("whats your name : ").title()
-url =f"https://api.genderize.io/?name={name}"
-gender_response = requests.get(url).text
-gender_dict = json.loads(gender_response)
-gender1 = gender_dict['name']
-country = gender_dict['gender']
-# variables and json use
-for key in gender_dict:
-    if {gender_dict[key]} == "age: None":
-     print("It is hard to guess you gender")
-    else:
-     print (f"{key}: {gender_dict[key]}")
 # printing dictionary
 Url =  f"https://api.nationalize.io/?name={name}"
 name_response = requests.get(Url).text
