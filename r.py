932871cd7430cbec1c0ac9810b223e6f32feea42
Druhi <druhi.vid@gmail.com>
2024-11-23 21:11:22 +0530

    to practise

--- r.py
+++ r.py
@@ -1,7 +1,7 @@
 import pyttsx3
 for i in range(2):
     engine = pyttsx3.init()
-    text= "happy birthday to you happy birthday to you happy birthday dear one happy birthday to you"
+    text= "happy birthday to you happy birthday to you happy birthday dear one happy birthday to you                       may god bless you may god bless you may gog bless you dear one happy birthday TO you"
     engine.say(text)
 
     engine.runAndWait()
