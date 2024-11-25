bb8be1558f67ac489a84ad87ecb6e39836614afe
Druhi <druhi.vid@gmail.com>
2024-11-25 21:10:54 +0530

    to practise

---
+++ 5-6.py
@@ -0,0 +1,14 @@
+age = int(input("how old are you - "))
+
+if age <= "2" :
+    print("you are a baby")
+elif "2" <= age <= "4" :
+    print("you are a toddler")
+elif "4" <= age <= "13" :
+    print("you are a kid")
+elif "13" <= age <= "20" :
+    print("you are a teen")
+elif "20" <= age <= "65" :
+    print("you are an adult")
+elif "65" <= age <= "100" :
+    print("you are an elder")
