6d5e52985cee4f9f35dc91096d49579651f23bf6
Druhi <druhi.vid@gmail.com>
2024-11-25 21:15:09 +0530

    to practise

--- turtle1.py
+++ turtle1.py
@@ -1,14 +1,18 @@
 import turtle
 # import
 screen = turtle.Screen()
-screen.bgcolor("white")
+screen.bgcolor("navy blue")
 pen = turtle.Turtle()
 # variable
 pen.speed(0)
-pen.fillcolor("green")
+pen.fillcolor("hot pink")
 pen.begin_fill()
 pen.circle(100)
 pen.end_fill()
+pen.shape("turtle")
+pen.back(30)
+pen.color("green")
+pen.circle(100)
 #settings
 turtle.done()
 # turtle.done make it steady
