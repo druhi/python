6237189c1efccc87cff17b84bcd88f39a1c6e9bd
Druhi <druhi.vid@gmail.com>
2024-11-25 21:10:06 +0530

    to practise

---
+++ 7-3.py
@@ -0,0 +1,5 @@
+number = int(input("Enter a number, and I'll tell you if it is multiple of ten - "))
+if number % 10 == 0:
+    print(f"The number {number} is multiple of ten.")
+else:
+    print(f"The number {number} is not a  multiple of ten.")
