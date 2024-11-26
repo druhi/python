1918b686574a02be1f826e5a635c375f28537679
Druhi <druhi.vid@gmail.com>
2024-11-26 20:32:41 +0530

    to practise lists

--- 3-4orielly.py
+++ 3-4orielly.py
@@ -8,3 +8,4 @@ guest.append("niece")
 print(f"dear{guest[0]} you are invited for our birthday")
 print(f"dear{guest[1]} you are invited for our birthday")
 print(f"dear{guest[2]} you are invited for our birthday")
+# guest list
