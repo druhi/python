e0bee64616cc193dcd8e73691f7ce2217eb4645c
Druhi <druhi.vid@gmail.com>
2024-11-26 20:32:27 +0530

    to practise lists

--- 3-3orielly.py
+++ 3-3orielly.py
@@ -2,3 +2,4 @@ guest = ["aunt","uncle","sister"]
 print(f"dear{guest[0]} you are invited for our birthday")
 print(f"dear{guest[1]} you are invited for our birthday")
 print(f"dear{guest[2]} you are invited for our birthday")
+# basic lists
