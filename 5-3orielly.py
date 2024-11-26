5cdc0432563a298d0e3683ee585a289285baa24f
Druhi <druhi.vid@gmail.com>
2024-11-26 20:34:55 +0530

    to practise

--- 5-3orielly.py
+++ 5-3orielly.py
@@ -1,6 +1,6 @@
 alien_color = ["red","green","yellow"]
 if alien_color == "green":
-    print("you have earned 5 points")
+    print("you have earned 5 points")#conditional tests
 if alien_color == "red":
     print()
     
