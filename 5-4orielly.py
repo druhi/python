ad4e886fe22b6580f045809fda085cf7089d2656
Druhi <druhi.vid@gmail.com>
2024-11-26 20:35:09 +0530

    to practise

--- 5-4orielly.py
+++ 5-4orielly.py
@@ -1,6 +1,6 @@
-alien_color = ["red","green","yellow"]
+alien_color = ["red","green","yellow"]#list
 if alien_color == "green":
-    print("you have earned 5 points")
+    print("you have earned 5 points")#conditions
 if alien_color >= "green":
     print("you have earned 10 points")
 
