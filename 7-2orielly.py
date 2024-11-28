46dcd5cb137516f4ccdea119b4592395ba0abe43
Druhi <druhi.vid@gmail.com>
2024-11-28 21:29:54 +0530

    to practise

--- 7-2orielly.py
+++ 7-2orielly.py
@@ -1,7 +1,8 @@
 import random
+# random is needed
 ask = input("how many people are there - ")
 r = ["1","2","3","4","5"]
 if ask >= "8":
     print("you will have to wait for a table")
 else:
-     print(f"table no {random.choice(r)}")
+     print(f"table no {random.choice(r)}")# and logic
