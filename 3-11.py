d601153ae1c51bef8ab1451e6f354aea81ea2709
Druhi <druhi.vid@gmail.com>
2024-12-01 20:09:53 +0530

    to be beware of index errors

---
+++ 3-11.py
@@ -0,0 +1,6 @@
+three = ["one" , "two" , "three"]
+# list
+print(three[3]) # list index starts from zero so zero is one first is two and the second one will be three as we want to print third one we cannot
+#3-11. Intentional Error: If you haven’t received an index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+
+
