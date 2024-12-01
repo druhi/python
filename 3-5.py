78e2c27ac41e9fd1cf9f7230515d629c167dc6c2
Druhi <druhi.vid@gmail.com>
2024-12-01 20:05:59 +0530

    to practise

---
+++ 3-5.py
@@ -0,0 +1,17 @@
+guest = ["aunt","uncle","sister"]
+print(f"dear{guest[0]} you are invited for our birthday")
+print(f"dear{guest[1]} you are invited for our birthday")
+print(f"dear{guest[2]} you are invited for our birthday")
+print("sister has imfomed she is not attending")
+guest.remove(guest[2])
+guest.append("niece")
+print(f"dear{guest[0]} you are invited for our birthday")
+print(f"dear{guest[1]} you are invited for our birthday")
+print(f"dear{guest[2]} you are invited for our birthday")
+print("dear sister we are so sorry for you please have a pleasant day")
+print(f"dear{guest[0]} you are invited for our birthday")
+print(f"dear{guest[1]} you are invited for our birthday")
+print(f"dear{guest[2]} you are invited for our birthday")
+# You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+# Start with your program from Exercise 3-4. Add a print() call at the end of your program, stating the name of the guest who can’t make it ..........................................
+
